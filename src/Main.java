216774376796d88ece25899b6e7b3616756dffdf
s11281 <s11281@pjwstk.edu.pl>
2013-09-28 12:42:24 +0200

    Update Main.java
    Test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 
 	/**
@@ -6,7 +5,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World 2013 HAHAHAHA");
 	}
 
 }
